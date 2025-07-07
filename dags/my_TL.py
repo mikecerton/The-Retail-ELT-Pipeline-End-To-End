9124945bc1cc7562bc92a4bbd05bc7997e3cfa06
Werapat Promwijit <werapat.pr@gmail.com>
2025-07-07 13:47:13 +0700

    Update my_TL.py

--- dags/my_TL.py
+++ dags/my_TL.py
@@ -6,11 +6,13 @@ import boto3
 import psycopg2
 from io import StringIO
 
-
+# upload data to S3
 def upload_s3(my_Fname, csv_buffer):
     
+    # Load data from .env file
     load_dotenv("/opt/airflow/.env")
 
+    # connect to s3
     bucket_name = os.getenv("bucket_name")
     session = boto3.Session(
         aws_access_key_id = os.getenv("aws_access_key_id"),
@@ -19,15 +21,19 @@ def upload_s3(my_Fname, csv_buffer):
 
     s3 = session.client('s3')
 
+    # Upload CSV to S3
     s3.put_object(Bucket=bucket_name, Key=my_Fname, Body=csv_buffer.getvalue())
     print(f"!_S3_upload_complete_{my_Fname}!")
 
+# copy data from S3 to data warehouse in Redshift
 def upload_redshift(my_Fname, table_name, sql_table):
 
+    # Load data from .env file
     load_dotenv("/opt/airflow/.env")
 
     bucket_name = os.getenv("bucket_name")
 
+    # Connect to Redshift
     conn = psycopg2.connect(
         host = os.getenv("redshift_host"),
         port = os.getenv("redshift_port"),
@@ -40,10 +46,12 @@ def upload_redshift(my_Fname, table_name, sql_table):
 
     cursor = conn.cursor()
 
+    # Create table if not exists
     cursor.execute(sql_table)
     conn.commit()
     print("table pass")
 
+    # Copy data from S3 to Redshif
     copy_query = f"""
     COPY {table_name}
     FROM '{s3_path}'
@@ -59,15 +67,15 @@ def upload_redshift(my_Fname, table_name, sql_table):
     cursor.close()
     conn.close()
 
-
+# ETL for dim_locations
 def location_Transform_Load():
 
     my_Fname = "location_data.csv"        
 
+    # create location data from csv
     df = pd.read_csv("/opt/airflow/my_data/my_retail_s3.csv")         
     csv_buffer = StringIO()
     regions = df[['Postal Code', 'Country', 'Region', 'State', 'City']].drop_duplicates(subset=['Postal Code'])
-    # print(regions.info())
     regions.to_csv(csv_buffer, index=False)
 
     sql_table = """
@@ -79,22 +87,19 @@ def location_Transform_Load():
         city VARCHAR(100) NOT NULL);
     """
 
+    # upload data to S3 --> create table if not exist --> copy data from S3 to data warehouse in Redshift
     upload_s3(my_Fname, csv_buffer)
-
     upload_redshift(my_Fname, "dim_locations", sql_table)
 
-
-
+# ETL for dim_products
 def product_Transform_Load():
 
     my_Fname = "product_data.csv"       
 
+    # create product data from csv
     df = pd.read_csv("/opt/airflow/my_data/my_retail_s3.csv")        
     csv_buffer = StringIO()
-
     product = df[['Product ID', 'Category', 'Sub-Category', 'Product Name']].drop_duplicates(subset=['Product ID'])
-    # print(product.info())
-
     product.to_csv(csv_buffer, index=False)
 
     sql_table = """
@@ -105,19 +110,19 @@ def product_Transform_Load():
         product_name VARCHAR(255));
     """
 
+    # upload data to S3 --> create table if not exist --> copy data from S3 to data warehouse in Redshift
     upload_s3(my_Fname, csv_buffer)
-
     upload_redshift(my_Fname, "dim_products", sql_table)
 
-
+# ETL for dim_customers
 def customer_Transform_Load():
 
     my_Fname = "customer_data.csv"       
 
+    # create customer data from csv
     df = pd.read_csv("/opt/airflow/my_data/my_retail_s3.csv")        
     csv_buffer = StringIO()
     customer = df[['Customer ID', 'Customer Name', 'Segment']].drop_duplicates(subset=['Customer ID'])
-    # print(customer.info())
     customer.to_csv(csv_buffer, index=False)
 
     sql_table = """
@@ -127,20 +132,21 @@ def customer_Transform_Load():
         segment VARCHAR(50));
     """
 
+    # upload data to S3 --> create table if not exist --> copy data from S3 to data warehouse in Redshift
     upload_s3(my_Fname, csv_buffer)
-
     upload_redshift(my_Fname, "dim_customers", sql_table)
 
+# ETL for dim_orders
 def order_Transform_Load():
 
     my_Fname = "order_data.csv"         
 
+    # create order data from csv
     df = pd.read_csv("/opt/airflow/my_data/my_retail_s3.csv")        
     csv_buffer = StringIO()
     order = df[['Order ID', 'Order Date', 'Ship Date', 'Ship Mode']].drop_duplicates(subset=['Order ID'])
     order['Order Date'] = pd.to_datetime(order['Order Date'], dayfirst=True)
     order['Ship Date'] = pd.to_datetime(order['Ship Date'], dayfirst=True)
-    # print(order.info())
     order.to_csv(csv_buffer, index=False)
 
     sql_table = """
@@ -151,14 +157,16 @@ def order_Transform_Load():
         ship_mode VARCHAR(50));
     """
 
+    # upload data to S3 --> create table if not exist --> copy data from S3 to data warehouse in Redshift
     upload_s3(my_Fname, csv_buffer)
-
     upload_redshift(my_Fname, "dim_orders", sql_table)
 
+# ETL for dim_time    
 def time_Transform_Load():
 
     my_Fname = "time_data.csv"         
-  
+    
+    # create time data from csv
     df = pd.read_csv("/opt/airflow/my_data/my_retail_s3.csv")          
     csv_buffer = StringIO()
     time = df['Order Date'].drop_duplicates()
@@ -172,7 +180,6 @@ def time_Transform_Load():
         'hour': time_new.dt.hour,
         'minute': time_new.dt.minute,
     })
-    # print(time.info())
     time.to_csv(csv_buffer, index=False)
 
     sql_table = """
@@ -186,19 +193,19 @@ def time_Transform_Load():
         minute INT NOT NULL);
     """
 
+    # upload data to S3 --> create table if not exist --> copy data from S3 to data warehouse in Redshift
     upload_s3(my_Fname, csv_buffer)
-
     upload_redshift(my_Fname, "dim_time", sql_table)
 
-
+# ETL for fact_profit_rep
 def ftc_profit_Transform_Load():
 
     my_Fname = "ftc_profit_data.csv"         
 
+    # create fact_profit_rep data from csv
     df = pd.read_csv("/opt/airflow/my_data/my_retail_s3.csv")          
     csv_buffer = StringIO()
     ftc_profit = df[['Order Date', 'Order ID', 'Customer ID', 'Product ID', 'Postal Code', 'Sales', 'Profit', 'Quantity', 'Discount', 'Segment', 'State', 'Region', 'Category']]
-    # print(ftc_profit.info())
     ftc_profit.to_csv(csv_buffer, index=False)
 
     sql_table = """
@@ -223,6 +230,7 @@ def ftc_profit_Transform_Load():
         FOREIGN KEY (customer_id) REFERENCES dim_customers(customer_id),
         FOREIGN KEY (postal_code) REFERENCES dim_locations(postal_code));
     """
-    upload_s3(my_Fname, csv_buffer)
 
+    # upload data to S3 --> create table if not exist --> copy data from S3 to data warehouse in Redshift
+    upload_s3(my_Fname, csv_buffer)
     upload_redshift(my_Fname, "fact_profit_rep", sql_table)
